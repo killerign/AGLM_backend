68c3ca2cab0128ac7de723b940a9e2921474af3d
killerign <srinivasananthu24@gmail.com>
2021-03-09 12:24:43 +0530

    take-10

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -2,6 +2,10 @@ const Post = require('../model/post_model');
  exports.showIndex = (req, res) => {
      Post.findOne({"uid": req.body.uid}) //fetches all the posts
      .then(result => {
+        res.append('Access-Control-Allow-Origin', ['*']);
+        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.append('Access-Control-Allow-Headers', 'Content-Type');
+        next();
          res.send(result);
      }).catch(err => {
          res.status(400).send(err);
