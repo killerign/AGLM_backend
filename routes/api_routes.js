bfb25fb06fb2735fa5ca3cdca88518c3f1dd6087
killerign <srinivasananthu24@gmail.com>
2021-03-09 12:41:19 +0530

    cola

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json();
 const postcontroller = require('../controllers/post_controller');
-const router = express.Router();
+const router = express();
 var cors = require('cors')
 router.post('/', jsonParser, cors() , postcontroller.showIndex);
 module.exports = router;
