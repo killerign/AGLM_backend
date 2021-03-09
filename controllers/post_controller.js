6f5d26ec1ad9e1e18ec250a4b262f8c1ed2e40cf
killerign <srinivasananthu24@gmail.com>
2021-03-09 12:38:38 +0530

    hola

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -2,9 +2,7 @@ const Post = require('../model/post_model');
  exports.showIndex = (req, res) => {
      Post.findOne({"uid": req.body.uid}) //fetches all the posts
      .then(result => {
-        res.append('Access-Control-Allow-Origin', ['*']);
-        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.append('Access-Control-Allow-Headers', 'Content-Type');
+        
         next();
          res.send(result);
      }).catch(err => {
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,5 +3,6 @@ var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json();
 const postcontroller = require('../controllers/post_controller');
 const router = express.Router();
-router.post('/', jsonParser, postcontroller.showIndex);
+var cors = require('cors')
+router.post('/', jsonParser, cors() , postcontroller.showIndex);
 module.exports = router;
