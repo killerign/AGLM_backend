d48da8ecb590339749c05c4a2c8e054a08399721
killerign <srinivasananthu24@gmail.com>
2021-03-09 11:18:15 +0530

    olla

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,5 +3,5 @@ var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json();
 const postcontroller = require('../controllers/post_controller');
 const router = express.Router();
-router.get('/', jsonParser, postcontroller.showIndex);
+router.post('/', jsonParser, postcontroller.showIndex);
 module.exports = router;
