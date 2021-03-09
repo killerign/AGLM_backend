ef4e025dc24ad6845dcd0808b9641335e5e409cb
killerign <srinivasananthu24@gmail.com>
2021-03-09 13:00:16 +0530

    colal

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,7 +1,7 @@
 const Post = require('../model/post_model');
- exports.showIndex = (req, res,next) => {
+ exports.showIndex = (req, res) => {
      console.log(req.body.uid);
-     Post.findOne({"uid": "cO77kwy785eXoSeoVgXgr81T3cq1"}) //fetches all the posts
+     Post.findOne({"uid": req.body.uid}) //fetches all the posts
      .then(result => {
         
         next();
